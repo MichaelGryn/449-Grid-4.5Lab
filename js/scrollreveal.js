6014130e8c1973ecc5c3cba6775a2d350f1a09cc
Michael Gryn <122385196+MichaelGryn@users.noreply.github.com>
2025-02-10 00:37:15 +0000

    Added grid on page, uses scroll reveal to load in, can't get reveal to work in js file instead of script tag

---
+++ js/scrollreveal.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    ScrollReveal().reveal('.module.one', {delay: 100});
+    ScrollReveal().reveal('.module.two', {delay: 200});
+    ScrollReveal().reveal('.module.three', {delay: 300});
+    ScrollReveal().reveal('.module.four', {delay: 400});
+    ScrollReveal().reveal('.module.five', {delay: 500});
+    ScrollReveal().reveal('.module.six', {delay: 600});
+    ScrollReveal().reveal('.module.seven', {delay: 700});
+    ScrollReveal().reveal('.module.eight', {delay: 800});
+    ScrollReveal().reveal('.module.nine', {delay: 900});
+});
